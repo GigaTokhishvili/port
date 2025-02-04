b51c5a91ace0c08d4d596041a75c3db55e8f67e6
Giga Tokhishvili <toxishvili123@gmail.com>
2025-02-05 00:10:13 +0400

    Removed covers and updated the switch button
    Removed covers (well technically turned them into comments in case I change my mind in the future). Played around with the switch button, there should be no more bugs and spamming the button should not break it anymore.

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const sun = document.querySelector('.sun');
-const moon = document.querySelector('.moon');
-const sunParent = document.querySelector('.cover-up');
-const moonParent = document.querySelector('.cover-down');
-const covers = document.querySelector('.covers');
+// const sun = document.querySelector('.sun');
+// const moon = document.querySelector('.moon');
+// const sunParent = document.querySelector('.cover-up');
+// const moonParent = document.querySelector('.cover-down');
+// const covers = document.querySelector('.covers');
 const theme = document.getElementById('theme');
 const switchDiv = document.getElementById('switch');
 const darkSwitch = document.getElementById('darkSwitch');
@@ -17,44 +17,72 @@ window.addEventListener('load', () => {
     })
 })
 
-// handling local storage
-if (localStorage.getItem('light') === 'true') {
-    covers.style.display = 'none';
 
-} else if (localStorage.getItem('dark') === 'true') {
-    theme.href = './dark.css';
-    darkSwitch.href = './darkSwitch.css';
-    covers.style.display = 'none';
+// running this cuz otherwise the sun svg animation does not work properly, as giving it an x value in css makes it jump from x:53 to x:0
+// window.onload = () => {
+//     if(localStorage.getItem('dark') === 'true') {
+//         gsap.set('sun', { x: 53 })
+//         gsap.set('moon', { x: 53, opacity: 1 })
+//     } else {
+//         gsap.set('sun', { x: 0 })
+//     }
+// }
+
+
+// handling local storage
+// if (localStorage.getItem('light') === 'true') {
+//     covers.style.display = 'none';
+
+// } else if (localStorage.getItem('dark') === 'true') {
+//     theme.href = './dark.css';
+//     darkSwitch.href = './darkSwitch.css';
+//     covers.style.display = 'none';
     
-} else {
-    document.body.classList.add('no-overflow');
+// } else {
+//     document.body.classList.add('no-overflow');
+// }
+
+window.onload = () => {
+
+    if(localStorage.getItem('light') === 'true') {
+        theme.href = '';
+        darkSwitch.href = '';
+    } else {
+        theme.href = './dark.css';
+        darkSwitch.href = './darkSwitch.css';
+        localStorage.setItem('dark', 'true');
+        gsap.set('#sun', { x: 53 })
+        gsap.set('#moon', { x: 0, opacity: 1,})
+        gsap.set('#moon_spots', { x: 53, opacity: 1, })
+        gsap.set('#clouds', { y: 130, })
+    } 
 }
 
 // handling theme switch
-sun.addEventListener('click', () => {
-    sunParent.classList.add('expand');
-    moonParent.classList.add('shrink');
-    covers.classList.add('fadeAway');
-    localStorage.setItem('light', 'true');
-    setTimeout(() => {
-        document.body.classList.remove('no-overflow');
-        covers.style.display = 'none';
-    }, 700)
-})
+// sun.addEventListener('click', () => {
+//     // sunParent.classList.add('expand');
+//     // moonParent.classList.add('shrink');
+//     // covers.classList.add('fadeAway');
+//     localStorage.setItem('light', 'true');
+//     setTimeout(() => {
+//         document.body.classList.remove('no-overflow');
+//         covers.style.display = 'none';
+//     }, 700)
+// })
 
-moon.addEventListener('click', () => {
-    moonParent.classList.add('expand');
-    sunParent.classList.add('shrink');
-    localStorage.setItem('dark', 'true');
-    localStorage.removeItem('light');
-    covers.classList.add('fadeAway');
-    theme.href = './dark.css';
-    darkSwitch.href = './darkSwitch.css';
-    setTimeout(() => {
-        document.body.classList.remove('no-overflow');  
-        covers.style.display = 'none';
-    }, 700)
-})
+// moon.addEventListener('click', () => {
+//     // moonParent.classList.add('expand');
+//     // sunParent.classList.add('shrink');
+//     localStorage.setItem('dark', 'true');
+//     localStorage.removeItem('light');
+//     // covers.classList.add('fadeAway');
+//     theme.href = './dark.css';
+//     darkSwitch.href = './darkSwitch.css';
+//     setTimeout(() => {
+//         document.body.classList.remove('no-overflow');  
+//         // covers.style.display = 'none';
+//     }, 700)
+// })
 
 
 function changeTheme() {
@@ -68,29 +96,34 @@ function changeTheme() {
         gsap.to('#stars', { duration: 1, y: 6, opacity: 1, ease: "power3.out",});
         
         theme.href = './dark.css';
+        localStorage.setItem('dark', 'true');
+        localStorage.removeItem('light');
         setTimeout(() => {
-            localStorage.setItem('dark', 'true');
-            localStorage.removeItem('light');
             darkSwitch.href = './darkSwitch.css';
         }, 700)
         
     } else {
-        gsap.to('#rect', {duration: 1, opacity: 1, ease: "power3.out"});
+        gsap.to('#rect', {duration: 1, opacity: 1, fill: '#357AB4', ease: "power3.out"});
         gsap.to('#rectDark', {duration: 1, opacity: 0, ease: "power3.out"});
         gsap.to('#sun', { duration: 1, x: 0, opacity: 1, ease: "power3.out",});
+        gsap.to('#moon', { duration: 1, x: 0, opacity: 1, ease: "power3.out",});
         gsap.to('#rays', { duration: 1, x: 0, opacity: 1, ease: "power3.out",});
         gsap.to('#moon_spots', { duration: 1, x: 0, opacity: 0, ease: "power3.out",});
         gsap.to('#clouds', { duration: 1, y: 0, ease: "power3.out",});
         gsap.to('#stars', { duration: 1, y: -120, ease: "power3.out",});
 
+        theme.href = '';
         localStorage.setItem('light', 'true');
         localStorage.removeItem('dark');
-        theme.href = '';
-        darkSwitch.href = '';
+        // darkSwitch.href = '';
+        setTimeout(() => {
+            darkSwitch.href = '';
+        }, 700);
     }
 }
 
 switchDiv.addEventListener('click', changeTheme);
+
 
 // scrolling 
 const home = document.getElementById('home');
